e8f05479f188879228b07b24ee343748ba7bad4c
Blessing Orazulume <blessing.orazulume@kindredgroup.com>
2019-09-27 11:34:02 +0200

    Add kindred logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,11 @@ const Header = () => {
     return(
         <header>
             <div className='header'>
-                <img className='headerImage' src="https://media.licdn.com/dms/image/C560BAQHFW_t5arQr4w/company-logo_400_400/0?e=1576713600&v=beta&t=ulJgxGS9tQX9faUZH7TZOGYwWuP_kMpvpHBA2hhh2hk"/>
+                <img
+                    className='headerImage'
+                    src="kindredLogo.jpeg"
+                    alt="kindredLogo"
+                />
                 <span className='headerTitle'>Kindred Twitter</span>
             </div>
         </header>
